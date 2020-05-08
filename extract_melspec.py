039d8b3e4ff686c88b142bff64ec4a0b02ec9fda
daniel-amin <danielamadeo20@gmail.com>
2020-05-08 03:28:10 -0400

    Added script for extracting mel-spec

---
+++ extract_melspec.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import os
+from tqdm import tqdm
+import librosa
+import warnings
+import sys
+import pickle
+import numpy as np
+
+def mp3gen(path):
+    for root, dirs, files in os.walk(path):
+        for filename in files:
+            if os.path.splitext(filename)[1] == ".mp3":
+                yield os.path.join(root, filename)
+                
+def fxn():
+    warnings.warn('User warning', UserWarning)
+    
+def main(path):
+    filenames = []
+    for mp3file in mp3gen(path):
+        filenames.append(mp3file)
+    
+    filenames = sorted(filenames, key=lambda x: (x.split('.')[0]))
+    mel_spec_data = []
+    
+    with warnings.catch_warnings():
+        warnings.simplefilter('ignore')
+        fxn()
+        for mp3 in tqdm(filenames):
+            try:
+                data, sampling_rate = librosa.load(mp3, mono=True)
+                mel_spec = librosa.feature.melspectrogram(y=data, sr=sampling_rate)
+                mel_spec_data.append(mel_spec)
+            except Exception as e:
+                print(e)
+                mel_spec_data.append(np.nan)
+                
+    return mel_spec_data
+
+
+if __name__ == "__main__":
+    mfcc = main(sys.argv[1])
+    
+    with open('mfcc.pkl', 'wb') as fp:
+        pickle.dump(mfcc, fp)
+
